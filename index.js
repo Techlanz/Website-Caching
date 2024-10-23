f5c8de075c8cbd805fd4c3040056147f04fffd1b
Abish-dev <abish.chhetri@techlanz.com>
2024-10-23 12:34:49 +0530

    changed the timings to 24hrs

--- index.js
+++ index.js
@@ -1,51 +1,45 @@
-// server.js
-
 const express = require('express');
 const NodeCache = require('node-cache');
 const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
-const cache = new NodeCache({ stdTTL: 100, checkperiod: 120 }); // stdTTL = 100 seconds, checkperiod = 120 seconds
 
-// Use CORS middleware to enable CORS for all routes
+const cache = new NodeCache({ stdTTL: 86400, checkperiod: 3600 });
+
 app.use(cors());
 
-// Sample data fetching function (replace with your actual API)
+
 const fetchDataFromAPI = async () => {
-  const response = await axios.get('https://script.google.com/macros/s/AKfycbyjMf_GNiIUUVLauy4vQrN0GLKdhZ_b0enf3mNxGoj49vaGLKX0DJ47RO14XhGk-7qX/exec'); // Placeholder API
+  const response = await axios.get('https://script.google.com/macros/s/AKfycbyjMf_GNiIUUVLauy4vQrN0GLKdhZ_b0enf3mNxGoj49vaGLKX0DJ47RO14XhGk-7qX/exec');
   return response.data; 
 };
 
+const cacheMiddleware = async (req, res, next) => {
+  const key = req.originalUrl;
+  let cachedData = cache.get(key);
 
-const cacheMiddleware = (req, res, next) => {
-  const key = req.originalUrl; 
-  const cachedData = cache.get(key);
 
-  if (cachedData) {
-    console.log('Serving from cache:', key);
-    return res.json(cachedData); 
-  } else {
-    next(); 
+  if (!cachedData) {
+    console.log('Cache is empty. Fetching new data...');
+    try {
+      cachedData = await fetchDataFromAPI(); 
+      cache.set(key, cachedData);          
+    } catch (error) {
+      return res.status(500).json({ error: 'Failed to fetch data from API' });
+    }
   }
+
+  console.log('Serving from cache:', key);
+  return res.json(cachedData);
 };
 
-// Route using cache
+
 app.get('/api/webinar/data', cacheMiddleware, async (req, res) => {
-  try {
-    const data = await fetchDataFromAPI();
 
-
-    cache.set(req.originalUrl, data);
-
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch data' });
-  }
 });
 
 const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
