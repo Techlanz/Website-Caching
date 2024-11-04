3b0479b7bf36d220199e1a90a8cd2f802904b2f3
Devank Gupta <software_intern1@techlanz.com>
2024-11-04 13:29:22 +0530

    converted to https with certificates

--- index.js
+++ index.js
@@ -1,44 +1,50 @@
-const express = require('express');
-const NodeCache = require('node-cache');
-const axios = require('axios');
-const cors = require('cors');
+const express = require("express");
+const NodeCache = require("node-cache");
+const axios = require("axios");
+const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
 
 const app = express();
-
 const cache = new NodeCache({ stdTTL: 86400, checkperiod: 3600 });
 
 app.use(cors());
 
-
 const fetchDataFromAPI = async () => {
-  const response = await axios.get('https://script.google.com/macros/s/AKfycbyjMf_GNiIUUVLauy4vQrN0GLKdhZ_b0enf3mNxGoj49vaGLKX0DJ47RO14XhGk-7qX/exec');
-  return response.data; 
+  const response = await axios.get(
+    "https://script.google.com/macros/s/AKfycbyjMf_GNiIUUVLauy4vQrN0GLKdhZ_b0enf3mNxGoj49vaGLKX0DJ47RO14XhGk-7qX/exec"
+  );
+  return response.data;
 };
 
 const cacheMiddleware = async (req, res, next) => {
   const key = req.originalUrl;
   let cachedData = cache.get(key);
 
-
   if (!cachedData) {
-    console.log('Cache is empty. Fetching new data...');
+    console.log("Cache is empty. Fetching new data...");
     try {
-      cachedData = await fetchDataFromAPI(); 
-      cache.set(key, cachedData);          
+      cachedData = await fetchDataFromAPI();
+      cache.set(key, cachedData);
     } catch (error) {
-      return res.status(500).json({ error: 'Failed to fetch data from API' });
+      return res.status(500).json({ error: "Failed to fetch data from API" });
     }
   }
 
-  console.log('Serving from cache:', key);
+  console.log("Serving from cache:", key);
   return res.json(cachedData);
 };
 
-
-app.get('/api/webinar/data', cacheMiddleware, async (req, res) => {
-});
+app.get("/api/webinar/data", cacheMiddleware, async (req, res) => {});
 
 const PORT = 3001;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+
+// Load self-signed certificate
+const options = {
+  key: fs.readFileSync("server.key"),
+  cert: fs.readFileSync("server.cert"),
+};
+
+https.createServer(options, app).listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on https://localhost:${PORT}`);
 });
