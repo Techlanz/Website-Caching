a02a56d31fc8ab937f203f382f48e93c7e7f62c4
Abish-dev <abish.chhetri@techlanz.com>
2024-11-11 23:37:13 +0530

    Auto refresh

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const https = require("https");
 const fs = require("fs");
 
 const app = express();
-const cache = new NodeCache({ stdTTL: 86400, checkperiod: 3600 });
+const cache = new NodeCache({ stdTTL: 0, checkperiod: 3600 }); 
 
 app.use(cors());
 app.use(express.json());
@@ -26,9 +26,11 @@ const passwordProtectionMiddleware = (req, res, next) => {
 
 const fetchDataFromAPI = async () => {
   try {
+   
     const response = await axios.get(
       "https://script.googleusercontent.com/macros/echo?user_content_key=Ia3NNolpzDpodAZNC78njI2A3XuIqoNtUuMsLxyg_PwxGt4OMEUifXoX0PjW4gDYKqcrkEHetNOU8GcUiFcXaZK68wV2Xrpcm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnHep0rK6S7k5BSWUi5sczp9UkV5Qbw7OJNJHmdEXpWQE_l3vH2pe06Aqrwq9ZkikIuGpsMsXKI-NuIEyKn5uKi0m9-RyHTObhw&lib=MgMlderQUy5a6rIvmCM6Y13NiaCb_EVGq"
     );
+   
     return response.data;
   } catch (error) {
     console.error("Failed to fetch data from API:", error.message);
@@ -37,17 +39,18 @@ const fetchDataFromAPI = async () => {
 };
 
 const cacheMiddleware = async (req, res, next) => {
-  const key = req.originalUrl;
+  const key = "/api/webinar/data";
   let cachedData = cache.get(key);
 
   if (cachedData) {
- 
+  
     return res.json(cachedData);
   } else {
-  
+   
     try {
       cachedData = await fetchDataFromAPI();
       cache.set(key, cachedData);
+      
       return res.json(cachedData);
     } catch (error) {
       return res.status(500).json({ error: "Failed to fetch data from API" });
@@ -55,32 +58,31 @@ const cacheMiddleware = async (req, res, next) => {
   }
 };
 
-app.get("/api/webinar/data", cacheMiddleware, async (req, res) => {});
-
-setInterval(() => {
-
-  fetchDataFromAPI()
-    .then((data) => {
+setInterval(async () => {
+  try {
+   
+    const data = await fetchDataFromAPI();
+    if (data) {
       cache.set("/api/webinar/data", data);
-    
-    })
-    .catch((error) => {
-      console.error("Failed to refresh cache:", error.message);
-    });
-}, 300000);
+      
+    } else {
+      console.warn("No data returned from fetchDataFromAPI");
+    }
+  } catch (error) {
+    console.error("Failed to refresh cache:", error.message);
+  }
+}, 900000); 
+
 
 app.get("/api/webinar/data", passwordProtectionMiddleware, cacheMiddleware);
 
 const PORT = 3001;
-
 
 const options = {
   key: fs.readFileSync("/etc/letsencrypt/live/dev.techlanz.com/privkey.pem"),
   cert: fs.readFileSync("/etc/letsencrypt/live/dev.techlanz.com/cert.pem"),
 };
 
-
 https.createServer(options, app).listen(PORT, "0.0.0.0", () => {
   console.log(`Secure server running on https://dev.techlanz.com:${PORT}`);
 });
-
