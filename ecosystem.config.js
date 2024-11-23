50de1c5d20a92182249ee51fc9d781770663f163
Devank Gupta <software_intern1@techlanz.com>
2024-11-23 15:39:45 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,11 +7,7 @@ module.exports = {
         NODE_ENV: "production", // Set environment to production
         PORT: 3001, // Set the port (match your server's port)
       },
-      // Optional: If you want PM2 to automatically restart your app on changes:
-      watch: true, // Enable watching for file changes (optional)
-      instances: 1, // Number of instances you want to run (e.g., 1)
-      autorestart: true, // Enable auto-restart for crashes
-      max_memory_restart: "1G", // Automatically restart if memory usage exceeds 1GB
+   max_memory_restart: "1G", // Automatically restart if memory usage exceeds 1GB
     },
   ],
 };
