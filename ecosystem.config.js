b0fb7fa13347a624b20bacb2aba75e3b015ed720
Devank Gupta <software_intern1@techlanz.com>
2024-11-23 15:55:00 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     {
       name: "Website-Caching", // Name of the app (can be anything you like)
       script: "./index.js", // Path to the entry file (e.g., server.js)
+      exec_mode: "fork",  // Make sure it's "fork" instead of "cluster"
       env: {
         NODE_ENV: "production", // Set environment to production
         PORT: 3001, // Set the port (match your server's port)
